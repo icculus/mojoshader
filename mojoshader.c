60316f3345c880794b49359fdf4d67996384a347
Ryan C. Gordon <icculus@icculus.org>
2008-05-05 22:17:17 -0400

    Patched to avoid compiler warning.
    --HG--
    branch : trunk

--- mojoshader.c
+++ mojoshader.c
@@ -2019,7 +2019,7 @@ static void emit_GLSL_end(Context *ctx)
 
 static void emit_GLSL_finalize(Context *ctx)
 {
-    const RegisterList *reg = NULL;
+    const RegisterList *reg;
 
     // throw some blank lines around to make source more readable.
     push_output(ctx, &ctx->globals);
@@ -2029,16 +2029,16 @@ static void emit_GLSL_finalize(Context *ctx)
     push_output(ctx, &ctx->mainline_intro);
     ctx->indent++;
 
-#if 0  // !!! FIXME: probably not necessary?
     // Make sure this is always set, moved from our generic attribute.
     reg = declared_attribute(ctx, MOJOSHADER_USAGE_POSITION, 0);
     if (reg != NULL)
     {
+#if 0  // !!! FIXME: probably not necessary?
         // !!! FIXME: only emit if shader didn't definitely set gl_Position.
         output_line(ctx, "gl_Position = %s;",
                             get_GLSL_varname(ctx, reg->regtype, reg->regnum));
-    } // if
 #endif
+    } // if
 
     ctx->indent--;
     pop_output(ctx);
