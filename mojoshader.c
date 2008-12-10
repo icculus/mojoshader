72fe350c3d13d501299a9f1a75c973cf4fa45451
Ryan C. Gordon <icculus@icculus.org>
2008-12-10 05:30:38 -0500

    Corrected state_IF() to handle IF PRED format.

--- mojoshader.c
+++ mojoshader.c
@@ -6160,8 +6160,11 @@ static void state_SINCOS(Context *ctx)
 
 static void state_IF(Context *ctx)
 {
-    if (ctx->source_args[0].regtype != REG_TYPE_CONSTBOOL)
-        fail(ctx, "IF src0 must be CONSTBOOL");
+    const RegisterType regtype = ctx->source_args[0].regtype;
+    if ((regtype != REG_TYPE_PREDICATE) && (regtype != REG_TYPE_CONSTBOOL))
+        fail(ctx, "IF src0 must be CONSTBOOL or PREDICATE");
+    else if (!replicate_swizzle(ctx->source_args[0].swizzle))
+        fail(ctx, "IF src0 must have replicate swizzle");
     // !!! FIXME: track if nesting depth.
 } // state_IF
 
