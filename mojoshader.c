8d12660c9910786babb457843ebd2d1a2a1f319d
Bart van der Werf <bluelive@gmail.com>
2020-06-03 10:58:37 -0400

    Fix vertex sampler reserved mask for 3D/cube textures

--- mojoshader.c
+++ mojoshader.c
@@ -1050,7 +1050,7 @@ static int parse_args_DCL(Context *ctx)
             const uint32 ttype = ((token >> 27) & 0xF);
             if (!valid_texture_type(ttype))
                 fail(ctx, "Unknown sampler texture type");
-            reserved_mask = 0x6FFFFFFF;
+            reserved_mask = 0x0FFFFFFF;
             ctx->dwords[0] = ttype;
         } // else if
         else
